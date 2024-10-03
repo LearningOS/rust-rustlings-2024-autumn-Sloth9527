c7e5166442f9cfd171c717496b30028af175ed4c
jk <940941188@qq.com>
2024-10-03 19:15:28 +0800

    feat: traits1

--- exercises/traits/traits1.rs
+++ exercises/traits/traits1.rs
@@ -15,6 +15,10 @@ trait AppendBar {
 
 impl AppendBar for String {
     // TODO: Implement `AppendBar` for type `String`.
+    fn append_bar(mut self) -> Self {
+        self.push_str("bar");
+        self
+    }
 }
 
 fn main() {
