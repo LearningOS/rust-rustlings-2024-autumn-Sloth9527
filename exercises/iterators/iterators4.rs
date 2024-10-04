9ca439cabf0c354bf947d79acef88c8312b723d0
jk <940941188@qq.com>
2024-10-04 20:50:27 +0800

    feat: iterators4

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -3,7 +3,7 @@
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
@@ -15,6 +15,12 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    std::iter::repeat(1)
+        .take(num as usize)
+        .enumerate()
+        .reduce(|(a, _), (b, _)| ((a+1) * (b+1) - 1, 1))
+        .or(Some((0, 1)))
+        .unwrap().0 as u64 + 1
 }
 
 #[cfg(test)]
